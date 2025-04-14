4a6f987a275001dacb03860715a4b11157d5de3b
Adithya Visnu Prasetyo Putra <adithyavisnu@gmail.com>
2025-04-14 16:44:53 +0700

    update bean test config primary

--- src/main/java/com/adithyavisnu/todolist/configuration/BeanDuplicateConfiguration.java
+++ src/main/java/com/adithyavisnu/todolist/configuration/BeanDuplicateConfiguration.java
@@ -4,10 +4,12 @@ import com.adithyavisnu.todolist.data.Foo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Slf4j
 @Configuration
 public class BeanDuplicateConfiguration {
+    @Primary
     @Bean
     public Foo foo1 () {
         return new Foo();
--- src/test/java/com/adithyavisnu/todolist/configuration/BeanDuplicateConfigurationTest.java
+++ src/test/java/com/adithyavisnu/todolist/configuration/BeanDuplicateConfigurationTest.java
@@ -12,9 +12,13 @@ public class BeanDuplicateConfigurationTest {
     void testBeanDuplicateConfiguration() {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanDuplicateConfiguration.class);
 
-        Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> {
-            Foo foo = applicationContext.getBean(Foo.class);
-        });
+        // as Bean annotated primary, the result would be primary
+        // otherwise it should assert throws because of NoUniqueBeanDefinitionException
+//        Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> {
+//            Foo foo = applicationContext.getBean(Foo.class);
+//        });
+
+        Foo foo = applicationContext.getBean(Foo.class);
     }
 
     @Test
@@ -25,5 +29,18 @@ public class BeanDuplicateConfigurationTest {
         Foo foo2 = applicationContext.getBean("foo2", Foo.class);
 
         Assertions.assertNotSame(foo1, foo2);
+    }
+
+    @Test
+    void testBeanDuplicateConfigurationPrimary() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanDuplicateConfiguration.class);
+        Foo foo1 = applicationContext.getBean(Foo.class);
+        Foo foo2 = applicationContext.getBean("foo2", Foo.class);
+
+        Foo foo = applicationContext.getBean("foo1", Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertSame(foo, foo1);
+        Assertions.assertNotSame(foo, foo2);
     }
 }
